488de6fecae479041dd902ab43493ad38dc625d9
Enes İnce <inceenes10@gmail.com>
2021-05-06 13:54:05 +0300

    Updated process.py

--- process.py
+++ process.py
@@ -1,37 +1,3 @@
-"""
-import torch
-import torchvision
-from torchvision import datasets, transforms
-from utils.settings import *
-import os
-import utils.yaml
-
-params = utils.yaml.read_yaml("params.yaml")
-
-
-
-labels = {
-    0: "MildDemented",
-    1: "ModerateDemented",
-    2: "NonDemented",
-    3: "VeryMildDemented"
-}
-
-transform = transforms.Compose([
-    transforms.ToTensor(),
-    transforms.Normalize(
-        params["mean"],
-        params["std"]
-        )
-    ])
-
-
-#os.rmdir(ALZHEIMER_PROCESSED_DATA_DIR)
-
-
-train_dataset = datasets.ImageFolder(TRAIN_RAW_DATA_DIR, transform=transform)
-train_set = torch.utils.data.DataLoader(train_dataset, batch_size=32, shuffle=False)"""
-
 import os
 from utils.settings import *
 import cv2
@@ -43,13 +9,7 @@ folders = {
     "test": ["MildDemented", "ModerateDemented", "NonDemented", "VeryMildDemented"]
 }
 
-def process(path, path_to_save):
-    filename = os.path.basename(path)
-    pass
-
-
 shutil.rmtree(ALZHEIMER_PROCESSED_DATA_DIR)
-
 
 for key, labels in folders.items():
     for label in labels:
