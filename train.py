08071aa5bb755acc6d2f6428b884009fde4bb961
Enes İnce <inceenes10@gmail.com>
2021-05-09 22:09:23 +0300

    bug fix

--- train.py
+++ train.py
@@ -4,9 +4,9 @@ import utils.yaml
 
 params = utils.yaml.read_yaml("params.yaml")
 
-image_width = params["image_width"]
-image_height = params["image_height"]
-batch_size = params["batch_size"]
+image_width = params["train"]["image_width"]
+image_height = params["train"]["image_height"]
+batch_size = params["train"]["batch_size"]
 
 train_ds = tf.keras.preprocessing.image_dataset_from_directory(
   ALZHEIMER_PROCESSED_DATA_DIR,
