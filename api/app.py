f5b60190de2c60a4a80200307f97a62bd38b504a
Enes İnce <inceenes10@gmail.com>
2021-05-15 14:42:57 +0300

    Updated api

--- api/app.py
+++ api/app.py
@@ -1,15 +1,14 @@
 import json
-
+import torch
 
 def predict(event, context):
     body = {
-        "message": "Go Serverless v1.0! Your function executed successfully!",
-        "input": event
+        "message": "Hello, world! Your function executed successfully!",
     }
 
     response = {
         "statusCode": 200,
         "body": json.dumps(body)
     }
-    
+
     return response
