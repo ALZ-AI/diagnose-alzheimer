316934f875fc45302b1f02a0d60c1b75317a8cd7
Enes İnce <inceenes10@gmail.com>
2021-05-22 19:53:59 +0300

    Updated PROJECT_ROOT_DIR variable to lastIndexOf

--- utils/settings.py
+++ utils/settings.py
@@ -1,7 +1,7 @@
 import os
 
 PROJECT_NAME = "diagnose-alzheimer"
-PROJECT_ROOT_DIR = os.getcwd()[:os.getcwd().index(PROJECT_NAME) + len(PROJECT_NAME)]
+PROJECT_ROOT_DIR = os.getcwd()[:os.getcwd().rindex(PROJECT_NAME) + len(PROJECT_NAME)]
 
 ALZHEIMER_RAW_DATA_DIR = os.path.join(PROJECT_ROOT_DIR, "data", "raw", "alzheimer")
 ALZHEIMER_PROCESSED_DATA_DIR = os.path.join(PROJECT_ROOT_DIR, "data", "processed", "alzheimer")
