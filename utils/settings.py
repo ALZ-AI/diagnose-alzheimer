eae96c8425ffcb494a4c633ee5111da2a57c654a
Enes İnce <inceenes10@gmail.com>
2021-05-29 20:33:07 +0300

    settings.py variables updated

--- utils/settings.py
+++ utils/settings.py
@@ -3,15 +3,6 @@ import os
 PROJECT_NAME = "diagnose-alzheimer"
 PROJECT_ROOT_DIR = os.getcwd()[:os.getcwd().rindex(PROJECT_NAME) + len(PROJECT_NAME)]
 
-ALZHEIMER_RAW_DATA_DIR = os.path.join(PROJECT_ROOT_DIR, "data", "raw", "alzheimer")
-ALZHEIMER_PROCESSED_DATA_DIR = os.path.join(PROJECT_ROOT_DIR, "data", "processed", "alzheimer")
-
-TRAIN_RAW_DATA_DIR = os.path.join(PROJECT_ROOT_DIR, "data", "raw", "alzheimer", "train")
-TEST_RAW_DATA_DIR = os.path.join(PROJECT_ROOT_DIR, "data", "raw", "alzheimer", "test")
-
-TRAIN_PROCESSED_DATA_DIR = os.path.join(PROJECT_ROOT_DIR, "data", "processed", "alzheimer", "train")
-TEST_PROCESSED_DATA_DIR = os.path.join(PROJECT_ROOT_DIR, "data", "processed", "alzheimer", "test")
-
 ADNI_RAW_DATA_DIR = os.path.join(PROJECT_ROOT_DIR, "data", "raw", "adni")
-ALZHEIMER_TRANSFORMED_DATA_DIR = os.path.join(PROJECT_ROOT_DIR, "data", "transformed", "adni")
+ADNI_TRANSFORMED_DATA_DIR = os.path.join(PROJECT_ROOT_DIR, "data", "transformed", "adni")
 ADNI_PROCESSED_DATA_DIR = os.path.join(PROJECT_ROOT_DIR, "data", "processed", "adni")
